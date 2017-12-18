422a1b8bd4be1c5cd2d14901234a51598d9d4114
Dan Robertson <dan@productintuition.com>
2017-12-17 22:08:38 -0800

    Extra Credit requirements
    * Added more properties to each quote object
    * Random change of background color (page and button) when quote changes
    * Automatically load new quote after 20 seconds
    * Copy edit to one of the quotes

--- js/script.js
+++ js/script.js
@@ -7,33 +7,43 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 
 var quotes = [
 	{
-		quote: "We *are* the committee.",
+		quote: "We <em>are</em> the committee.",
 		source: "Lord Birkenhead",
 		citation: "Chariots of Fire",
-		year: "1981"
+		year: "1981", 
+		tags: ["movies", "drama"],
+		sourceVerified: false
 	}, 
 	{
 		quote: "What the f*ck are you talking about, Ken?",
 		source: 'Johnny "Drama" Chase',
 		citation: "Entourage (Ep. 309)",
-		year: "2006"
+		year: "2006", 
+		tags: ["tv", "comedy", "f*ck"],
+		sourceVerified: false
 	}, 
 	{
 		quote: "Get the f*ck out of my face!",
 		source: "Pete Holmes",
 		citation: "You Made It Weird",
+		tags: ["podcasts", "comedy", "f*ck"],
+		sourceVerified: false
 	},
 	{
 		quote: "Of course it is happening inside your head, Harry, but why on Earth should that mean that it is not real?",
 		source: 'Albus Dumbledore',
 		citation: "Harry Potter and the Deathly Hallows",
-		year: "2007"
+		year: "2007", 
+		tags: ["books", "fiction"],
+		sourceVerified: true
 	},
 	{
 		quote: "Those little things that will make you successful in life... Things that are ridiculously easy to do—but just as easy not to do.",
 		source: 'Jeff Olson',
 		citation: "The Slight Edge: Turning Simple Disciplines into Massive Success and Happiness",
-		year: "2011"
+		year: "2011", 
+		tags: ["books", "nonfiction"],
+		sourceVerified: true
 	}
 ];
 
@@ -44,6 +54,23 @@ function getRandomQuote () {
 	return quotes[randomQuoteIndex];
 };
 
+// How to get random RGB value
+function getRandomRGB () {
+	var randomRGB = Math.floor( Math.random() * 256 );
+	return randomRGB;
+}
+
+// How to get random RGB color
+function getRandomColor () {
+	randomRed = getRandomRGB();
+	randomGreen = getRandomRGB();
+	randomBlue = getRandomRGB();
+
+	randomColor = "rgb(" + randomRed + ", " + randomGreen + ", " + randomBlue + ")";
+
+	return randomColor;
+};
+
 // How to print HTML for a random quote
 function printQuote () {
 	var randomQuote = getRandomQuote();
@@ -52,6 +79,7 @@ function printQuote () {
 	var citation = randomQuote["citation"];
 	var year = randomQuote["year"];
 
+	var randomColor = getRandomColor();
 	var quoteHTML = "";
 
 	// add HTML for required attributes
@@ -68,9 +96,17 @@ function printQuote () {
 	// close paragraph
 	quoteHTML += '</p>';
 
+	// Set random background colors for page body and button
+	document.body.style.backgroundColor = randomColor;
+	document.getElementById('loadQuote').style.backgroundColor = randomColor;
+
+	// Print the quote
 	document.getElementById('quote-box').innerHTML = quoteHTML;
 }
 
 // Print randomm quote on first page load
 printQuote();
+
+// Refresh quote every 30 seconds
+setInterval (printQuote, 20000);
 
